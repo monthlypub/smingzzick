683774159f1945319342c5985c13b4f33619f3c0
monthlypub <since8282@gmail.com>
2019-02-24 19:40:54 +0900

    Add files via upload

--- docs/sw.js
+++ docs/sw.js
@@ -1,14 +1,14 @@
 var cacheName = 'wof_cache';
 var appShellFiles = [
   '/wof.html',
-  '/res/',
-  '/res/BMJUA_otf.otf',
-  '/res/favicon_16x16.png',
-  '/res/favicon_32x32.png',
-  '/res/128x128.png',
-  '/res/144x144.png',
-  '/res/152x152.png',
-  '/res/192x192.png'
+  '/wof_res/',
+  '/wof_res/BMJUA_otf.otf',
+  '/wof_res/favicon_16x16.png',
+  '/wof_res/favicon_32x32.png',
+  '/wof_res/128x128.png',
+  '/wof_res/144x144.png',
+  '/wof_res/152x152.png',
+  '/wof_res/192x192.png'
 ];
 
 self.addEventListener('install', function(e) {
